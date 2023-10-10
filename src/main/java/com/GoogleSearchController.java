36b7b0ac0362634b233e760028f0654115724011
Ritik <36955147+The-Flash-Routine@users.noreply.github.com>
2023-10-10 12:53:59 +0100

    Add files via upload

---
+++ src/main/java/com/GoogleSearchController.java
@@ -0,0 +1,19 @@
+package com;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+public class GoogleSearchController {
+    @Autowired
+    private GoogleSearchService googleSearchService;
+
+    @GetMapping("/search")
+    public String search(@RequestParam String query) throws IOException {
+        return googleSearchService.scrapeFirstLinkFromGoogleSearch(query);
+    }
+}
